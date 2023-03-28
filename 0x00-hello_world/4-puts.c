20cdee58c5f328d05f94541b7085d1aad61f664d
55Nights <1905587@students.kca.ac.ke>
2023-03-28 19:40:04 +0300

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	char *s = "\"Programming is like building a multilingual puzzle,";
+	char *s = "\"Programming is like building a multilingual puzzle,\n";
 
 	puts(s);
 	return (0);
