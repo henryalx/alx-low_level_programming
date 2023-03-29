f076b780e6dd0ef82e79eeeac9812848221372ce
55Nights <1905587@students.kca.ac.ke>
2023-03-29 09:16:12 +0300

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,");
+	printf("\n");
+	return (0);
+}
